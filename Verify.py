d5ab202712d890ca170b1e9c75da4e21d20fbfb7
MohammadRezaQaderi <Mrq112775@outlook.com>
2021-08-28 11:51:32 +0430

    New edit for installation

--- Verify.py
+++ Verify.py
@@ -4,7 +4,7 @@ import numpy
 from mmaction.apis import init_recognizer, inference_recognizer
 
 config_file = 'configs/recognition/tsn/tsn_r50_video_inference_1x1x3_100e_kinetics400_rgb.py'
-device = 'cuda:0' # or 'cpu'
+device = 'cuda:10.2' # or 'cpu'
 device = torch.device(device)
 
 model = init_recognizer(config_file, device=device)
